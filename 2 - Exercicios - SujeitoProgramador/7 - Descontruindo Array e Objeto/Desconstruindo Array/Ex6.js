5d9dd1064ec357a03f16c75d26822d76f987d7c5
Pedro Augusto <pe.augusto2408@gmail.com>
2026-06-16 21:40:06 -0300

    Atualização 16/06

---
+++ 2 - Exercicios - SujeitoProgramador/7 - Descontruindo Array e Objeto/Desconstruindo Array/Ex6.js
@@ -0,0 +1,24 @@
+// let frutas = ["Maça", "Goiaba", "Jabuticaba", "Carambola"]
+
+// const [primeira, , terceira] = frutas
+
+// console.log(primeira, terceira)
+
+
+// let valores = [1, 2, 3, 4, 5]
+
+// let [primeiro, segundo, ...resto] = valores
+
+// console.log(primeiro, segundo, resto)
+
+// let cores = ["vermelho", "azul", "verde", "preto"]
+
+// let {2:terceiro} = cores
+
+// console.log(terceiro)
+
+let dados = [10, 20, 30, 40, 50];
+
+let [,, terceiro] = dados;
+
+console.log(terceiro);
